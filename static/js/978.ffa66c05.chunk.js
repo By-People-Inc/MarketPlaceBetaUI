18f922c63725c3623b73006acb61828d112274aa
Chandra Mohan Jha <imcmjha@protonmail.com>
2022-01-29 18:28:26 +0530

    Added home page & login page

---
+++ static/js/978.ffa66c05.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkmarketplace_frontend=self.webpackChunkmarketplace_frontend||[]).push([[978],{9978:function(e,n,t){t.r(n);t(2791);var r=t(184);n.default=function(){return(0,r.jsx)("div",{style:{fontSize:"2rem",textAlign:"center",marginTop:"2rem"},children:"Coming soon."})}}}]);
+//# sourceMappingURL=978.ffa66c05.chunk.js.map
